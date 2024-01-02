cb5dd087eea335ccc956571a57461acda722941e
mangbudiarta <17.inyomanbudiarta@gmail.com>
2024-01-02 16:48:36 +0800

    fix modal name

--- resources/views/admin/footer/datafooter.blade.php
+++ resources/views/admin/footer/datafooter.blade.php
@@ -1,13 +1,12 @@
 {{-- tempat alert notif --}}
 <div class="alert-notif"></div>
 <table class="table border-top">
-        {{-- Jika jumlah footer == 0, tampilkan tombol footer lainnya --}}
-            @if (count($footer) == 0)
-                <button type="button" class="btn btn-primary mb-4" data-bs-toggle="modal"
-                    data-bs-target="#FooterTambah">
-                    <i class="menu-icon tf-icons bx bx-plus m-0"></i>Tambah
-                </button>
-            @endif
+    {{-- Jika jumlah footer == 0, tampilkan tombol footer lainnya --}}
+    @if (count($footer) == 0)
+        <button type="button" class="btn btn-primary mb-4" data-bs-toggle="modal" data-bs-target="#footerTambah">
+            <i class="menu-icon tf-icons bx bx-plus m-0"></i>Tambah
+        </button>
+    @endif
     <thead>
         <tr>
             <th>No</th>
@@ -22,8 +21,8 @@
                 <td>{{ $loop->iteration }}</td>
                 <td>{{ $item->deskripsi }}</td>
                 <td>
-                    <img src="/storage/footer_img/{{ $item->gambar }}" alt="Gambar Footer" width="100"
-                        height="100" class="img-fluid img-thumbnail">
+                    <img src="/storage/footer_img/{{ $item->gambar }}" alt="Gambar Footer" width="100" height="100"
+                        class="img-fluid img-thumbnail">
                 </td>
                 <td>
                     <a href="#" id='{{ $item->id_footer }}' class="btn btn-warning btn-sm editfooter"
